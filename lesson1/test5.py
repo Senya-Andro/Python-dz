404bec2576aba5e1da843e82fdc3aab82923ab16
Senya.Andro <ksenix.r@gmail.com>
2025-01-11 16:44:36 +0300

    lesson2

---
+++ lesson1/test5.py
@@ -0,0 +1,14 @@
+def print_letter(num):
+    print(num)
+
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
+print_letter(88005553535)
