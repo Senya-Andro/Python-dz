5f051e11eb5d86f889974640c984edb73d972c26
Senya.Andro <ksenix.r@gmail.com>
2025-01-12 22:39:10 +0300

    lesson2

--- lesson1/lesson_1_task_3.py
+++ lesson1/lesson_1_task_3.py
@@ -1,5 +1,4 @@
 first_name = input('Введите ваше имя: ')
-print('Привет, ' + first_name)
-
 last_name = input('Введите вашу фамилию: ')
-print("Вас зовут: " + last_name, first_name)
+
+print("Привет, " + first_name, last_name)
