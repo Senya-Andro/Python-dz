b198da749b2c66ea78907993650899be22254497
Senya.Andro <ksenix.r@gmail.com>
2025-02-20 22:30:45 +0300

    lesson2

--- 06_lesson/Дождаться картинки.py
+++ 06_lesson/Дождаться картинки.py
@@ -1,27 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+chrome_options = webdriver.ChromeOptions()
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Переход на сайт
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-# Ожидание загрузки всех картинок
-WebDriverWait(driver, 20).until(
-  EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#image-container img"))
-)
+waiter = WebDriverWait(driver, 30)
 
-# Получение атрибута src у 3-й картинки
-images = driver.find_elements(By.CSS_SELECTOR, "#image-container img")  # Находим все изображения
+waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#award")))
 
-if len(images) >= 3:  # Проверяем, что есть хотя бы 3 картинки
-    src_attribute = images[2].get_attribute("src")
-    print(f"Атрибут src у 3-й картинки: {src_attribute}")
-else:
-    print(f"Найдено изображений: {len(images)}")
-
-driver.quit()
+print(driver.find_element(By.CSS_SELECTOR, "#award").get_dom_attribute("src"))
